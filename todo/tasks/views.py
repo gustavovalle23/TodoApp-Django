3c9dd2b968d7f65b9b7e6e5858ca14ca78287b9b
Gustavo Valle <gustavo.vallerp26@gmail.com>
2020-08-01 17:11:18 -0300

    Post a task

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from .models import *
+from .forms import *
 
 def index(request):
-    return render(request, 'tasks/list.html')
+    tasks = Task.objects.all()
+    form = TaskForm()
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    
+    context = {'tasks':tasks, 'form':form}
+    return render(request, 'tasks/list.html', context)
+
+# def updateTask(request, pk):
+    # task = Task.objects.get(id=pk)
+
+    # return render(request, 'tasks/update_tasks.html')
